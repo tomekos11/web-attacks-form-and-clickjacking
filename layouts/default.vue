1f9e73046a919971e2746779d08d33020f78d9be
Tomasz Slapinski <tom.slapinski@gmail.com>
2025-05-07 16:15:37 +0200

    +init

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <header>
+        <h1>My App</h1>
+        <nav>
+          <!-- Nawigacja -->
+        </nav>
+      </header>
+      <main>
+        <NuxtPage />
+      </main>
+      <footer>
+        <!-- Stopka -->
+        <p>© 2025 MyApp</p>
+      </footer>
+    </div>
+  </template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h2>Witaj w aplikacji!</h2>
+        <p>To jest strona główna.</p>
+  </div>
+</template>
